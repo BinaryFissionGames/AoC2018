9f6e540c3829ad827213529d4da2fe0c7b231152
Brandon Johnson <zebrahome10@gmail.com>
2018-12-01 01:45:11 -0500

    Comment and tweak Day1 code to be more readable.

--- Day1/part1/solution.js
+++ Day1/part1/solution.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 
 let input = fs.readFileSync("input.txt", {encoding: 'utf8'});
-let result = input.split('\n')
-            .map( x => Number.parseInt(x[0] == '+' ? x.substring(1) : x, 10))
-            .filter(x => x != NaN)
-            .slice(0, -1)
-            .reduce((acc, x) => acc + x, 0);
+
+let result = input.split('\n') // Create an a array where each element is a line
+            .map( x => Number.parseInt(x[0] == '+' ? x.substring(1) : x, 10)) //Map each string to its corresponding integer
+            .filter(x => !isNaN(x)) //Filter off NaN entries (probably from trailing space at the end of the file)
+            .reduce((acc, x) => acc + x, 0); // Get the sum of all numbers
+
 console.log(result);
--- Day1/part2/solution.js
+++ Day1/part2/solution.js
@@ -1,23 +1,30 @@
 const fs = require("fs");
 
 let input = fs.readFileSync("input.txt", {encoding: 'utf8'});
+//Same as part one, except we don't sum the array
 let nums = input.split('\n')
             .map( x => Number.parseInt(x[0] == '+' ? x.substring(1) : x, 10))
-            .filter(x => x != NaN)
-            .slice(0, -1);
+            .filter(x => !isNaN(x));
 
 let curFrequency = 0;
-let prevFrequencies = {0: 1}
-while(true){
+// Map from a frequency to the number of times it's been hit already.
+//An undefined entry implicitly mean it has been hit 0 times.
+let prevFrequencies = {0: 1}  
+
+//While we haven't hit the current frequency at least twice...
+while(prevFrequencies[curFrequency] < 2){
+    //Iterate of every number in our frequency loop
     for(let num of nums){
         curFrequency += num;
         if(prevFrequencies[curFrequency] === undefined){
+            //Frequency hasn't been hit before
             prevFrequencies[curFrequency] = 1;
         }else{
-            prevFrequencies[curFrequency] = 2;
-            break;
+            //Frequency has been hit before
+            prevFrequencies[curFrequency] += 1;
+            //Break if we have seen this frequency 2 or more times
+            if(prevFrequencies[curFrequency] >= 2) break;
         }
     }
-    if(prevFrequencies[curFrequency] === 2) break;
 }
 console.log(curFrequency);
